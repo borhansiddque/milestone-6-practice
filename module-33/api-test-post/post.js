d711a5aa0065fa17577cd0cee1bfbb674d057a26
Risan <borhansiddque19@gmail.com>
2025-03-10 03:16:35 +0600

    Enhance post display functionality by adding modal for post details in post.js

--- module-33/api-test-post/post.js
+++ module-33/api-test-post/post.js
@@ -1,24 +1,36 @@
 const getPhoto = () => {
-  fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(Response => Response.json())
-  .then(data => {
-    showImage(data)
-    
-  })
-}
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((Response) => Response.json())
+    .then((data) => {
+      showImage(data);
+    });
+};
 
 const showImage = (posts) => {
-  const postsContainer = document.getElementById('postsContainer');
+  const postsContainer = document.getElementById("postsContainer");
   for (const post of posts) {
-    const div = document.createElement('div');
-    div.classList.add('text-center')
+    const postId = post.id;
+    
+    const div = document.createElement("div");
+    div.classList.add("text-center");
     div.innerHTML = `
       <h3 class="text-3xl font-semibold">${post.title}</h3>
       <p class="text-xl text-gray-500">${post.body}</p>
-      <button class="btn">Details</button>
-    `
+      <button class="btn" onclick="document.getElementById('${postId}').showModal()">Details</button>
+      <dialog id="${postId}" class="modal">
+        <div class="modal-box">
+          <h3 class="text-lg font-bold">${post.title}</h3>
+          <p class="py-4">${post.body}</p>
+          <div class="modal-action">
+            <form method="dialog">
+              <button class="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    `;
     postsContainer.append(div);
   }
-}
+};
 
-getPhoto()
+getPhoto();
