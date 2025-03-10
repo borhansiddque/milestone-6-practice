e841b55ef99bce175d3cdca9e97f30fa8f308123
Risan <borhansiddque19@gmail.com>
2025-03-10 14:54:28 +0600

    Add buttons for loading data and users, and implement fetch functions in fetch.js and moreAPI.js

--- module-33/module practice/scripts/fetch.js
+++ module-33/module practice/scripts/fetch.js
@@ -1,3 +1,9 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
+
+
+const handleLoadData = () => {
+      fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then(res => res.json())
+      .then(data => {
+            console.log(data); 
+      })
+}
