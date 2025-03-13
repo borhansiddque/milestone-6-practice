33e07b69d9255efb8e6d7daa3a5f3872e3dd2489
Risan <borhansiddque19@gmail.com>
2025-03-13 14:49:23 +0600

    Add category button section and fetch categories from API in index.html and index.js

---
+++ b10a6-peddy-website-design/scripts/index.js
@@ -0,0 +1,26 @@
+const loadCategoryBtn = async () => {
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/peddy/categories"
+  );
+  const data = await response.json();
+  showCategory(data.categories);
+};
+
+loadCategoryBtn();
+
+const showCategory = (category) => {
+  const categoryBtnContainer = document.getElementById("category-btn");
+  category.forEach((element) => {
+    console.log(element);
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <button
+      class="flex gap-4 items-center text-2xl font-bold primary-color py-6 px-20 border border-transparent hover:border-[#0E7A81] bg-[#0E7A8110] rounded-full cursor-pointer"
+    >
+      <img src="./images/shiba-1.png" alt="">
+      ${element}
+    </button>
+    `;
+    categoryBtnContainer.append(div);
+  });
+};
